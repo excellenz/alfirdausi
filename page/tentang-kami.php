9b5ea3d88ef25eba714ea9efd56340b7f420e723
Excellenz <63463920+jaruki2014@users.noreply.github.com>
2020-12-30 13:00:04 +0700

    edit about

--- page/tentang-kami.php
+++ page/tentang-kami.php
@@ -1,31 +1,50 @@
 <!--sevices-->
 <div class="advantages">
-	<div class="container">
-		<div class="advantages-main">
-				<h3 class="title-w3-agileits">Our Services</h3>
-		   <div class="advantage-bottom">
-			 <div class="col-md-6 advantage-grid left-w3ls wow bounceInLeft" data-wow-delay="0.3s">
-			 	<div class="advantage-block ">
-					<i class="fa fa-credit-card" aria-hidden="true"></i>
-			 		<h4>Stay First, Pay After! </h4>
-			 		<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates.</p>
-					<p><i class="fa fa-check" aria-hidden="true"></i>Decorated room, proper air conditioned</p>
-					<p><i class="fa fa-check" aria-hidden="true"></i>Private balcony</p>
-			 		
-			 	</div>
-			 </div>
-			 <div class="col-md-6 advantage-grid right-w3ls wow zoomIn" data-wow-delay="0.3s">
-			 	<div class="advantage-block">
-					<i class="fa fa-clock-o" aria-hidden="true"></i>
-			 		<h4>24 Hour Restaurant</h4>
-			 		<p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates.</p>
-					<p><i class="fa fa-check" aria-hidden="true"></i>24 hours room service</p>
-					<p><i class="fa fa-check" aria-hidden="true"></i>24-hour Concierge service</p>
-			 	</div>
-			 </div>
-			<div class="clearfix"> </div>
-			</div>
-		</div>
-	</div>
+  <div class="container">
+    <div class="advantages-main">
+      <h3 class="title-w3-agileits">Lokasi Penginapan Alfirdausi
+      </h3>
+      <div class="advantage-bottom">
+        <div class="col-md-6 advantage-grid left-w3ls wow bounceInLeft" data-wow-delay="0.3s">
+          <div class="advantage-block ">
+            <i class="fa fa-credit-card" aria-hidden="true">
+            </i>
+            <h4>Area Ponpes Husnul Khotimah 
+            </h4>
+            <p>Kamar tipe A, G, H , I, J dan K  adalah terletak  di dalam  area ponpes Husnul Khotimah 1, dari HK MART PUTRI ke arah timur kira-kira 10 m sebelah kanan jalan, sebelah barat bangunan RUMAH SEHAT  yang baru, kira-kira 15 meter dari gerbang utama ponpes  Al Multazam 1.
+            </p>
+            <p>
+              <i class="fa fa-check" aria-hidden="true">
+              </i>Terletak di dalam area ponpes HK
+            </p>
+            <p>
+              <i class="fa fa-check" aria-hidden="true">
+              </i>Prioritas Untuk Wali Santri HK
+            </p>
+          </div>
+        </div>
+        <div class="col-md-6 advantage-grid right-w3ls wow zoomIn" data-wow-delay="0.3s">
+          <div class="advantage-block">
+            <i class="fa fa-clock-o" aria-hidden="true">
+            </i>
+            <h4>Luar Area Ponpes Husnul Khotimah
+            </h4>
+            <p>Kamar tipe B, C, D , E ,  F, L dan M  adalah terletak  di luar area pondok pesantren Husnul Khotimah 1, dekat jembatan dan lapangan sepak bola, kira-kira 80 meter sebelum gerbang depan ponpes  Husnul Khotimah 1 atau kira kira 100 meter dari gerbang utama ponpes  Al Multazam 1.
+            </p>
+            <p>
+              <i class="fa fa-check" aria-hidden="true">
+              </i>Dekat Ponpes HK 1 dan Al Multazam 1
+            </p>
+            <p>
+              <i class="fa fa-check" aria-hidden="true">
+              </i>Prioritas Untuk Wali Santri Multazam
+            </p>
+          </div>
+        </div>
+        <div class="clearfix"> 
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 <!--//sevices-->
