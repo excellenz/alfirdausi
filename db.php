682871110257406833452859a784ed89f47831e1
Excellenz <63463920+jaruki2014@users.noreply.github.com>
2021-01-09 15:59:15 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost","root","","hotel") or die(mysql_error());
+$con = mysqli_connect("localhost","alfi_hotel","h4k4n3kunXXX","alfi_hotel") or die(mysql_error());
 
-?>
+?> 
